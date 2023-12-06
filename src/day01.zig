3c0a52cf9f44a707dec6f8f5ebbcbe42a2c91e5f
Lewis Gaul <>
2023-12-06 20:14:50 +0000

    Day 1 part 1 done

--- src/day01.zig
+++ src/day01.zig
@@ -8,6 +8,7 @@ const List = std.ArrayList;
 const Map = std.AutoHashMap;
 const StrMap = std.StringHashMap;
 const BitSet = std.DynamicBitSet;
+const ParseIntError = std.fmt.ParseIntError;
 
 const util = @import("util.zig");
 const gpa = util.gpa;
@@ -31,6 +32,7 @@ const trim = std.mem.trim;
 const sliceMin = std.mem.min;
 const sliceMax = std.mem.max;
 
+const charToDigit = std.fmt.charToDigit;
 const parseInt = std.fmt.parseInt;
 const parseFloat = std.fmt.parseFloat;
 
@@ -41,7 +43,64 @@ const sort = std.sort.block;
 const asc = std.sort.asc;
 const desc = std.sort.desc;
 
+const isDigit = std.ascii.isDigit;
+
+const Str = []const u8;
+
+const FindDirection = enum { start, end };
+
+const LineIter = struct {
+    _iter: std.mem.SplitIterator(u8, std.mem.DelimiterType.scalar),
+
+    const Self = @This();
+
+    pub fn init(input: Str) Self {
+        return .{ ._iter = std.mem.splitScalar(u8, input, '\n') };
+    }
+
+    pub fn next(self: *Self) ?Str {
+        if (self._iter.next()) |line| {
+            const trimmed = std.mem.trim(u8, line, " \t\r");
+            if (trimmed.len == 0) return self.next();
+            return trimmed;
+        } else {
+            return null;
+        }
+    }
+};
+
+fn findDigit(line: Str, direction: FindDirection) error{NoDigitFound}!u8 {
+    for (0..line.len) |i| {
+        const char = if (direction == .start) line[i] else line[line.len - i - 1];
+        return charToDigit(char, 10) catch continue;
+    }
+    return error.NoDigitFound;
+}
+
+fn part1(input: Str) error{NoDigitFound}!u32 {
+    var answer: u32 = 0;
+    var iter = LineIter.init(input);
+    while (iter.next()) |line| {
+        const first_digit = try findDigit(line, .start);
+        const last_digit = try findDigit(line, .end);
+        answer += 10 * first_digit + last_digit;
+    }
+    return answer;
+}
 
 pub fn main() !void {
-    print("Running day01...\n", .{});
+    try util.runPart(part1, data);
+    print("\n", .{});
+    // try util.runPart(part2, data);
+}
+
+test "part1" {
+    const input =
+        \\1abc2
+        \\pqr3stu8vwx
+        \\a1b2c3d4e5f
+        \\treb7uchet
+    ;
+    const answer = try part1(input);
+    assert(answer == 142);
 }
--- src/util.zig
+++ src/util.zig
@@ -9,8 +9,6 @@ const Str = []const u8;
 var gpa_impl = std.heap.GeneralPurposeAllocator(.{}){};
 pub const gpa = gpa_impl.allocator();
 
-// Add utility functions here
-
 // Useful stdlib functions
 const tokenizeAny = std.mem.tokenizeAny;
 const tokenizeSeq = std.mem.tokenizeSequence;
@@ -37,3 +35,19 @@ const assert = std.debug.assert;
 const sort = std.sort.block;
 const asc = std.sort.asc;
 const desc = std.sort.desc;
+
+pub fn runPart(comptime part: fn (Str) anyerror!u32, data: Str) !void {
+    const start = std.time.nanoTimestamp();
+    const answer = try part(data);
+    const end = std.time.nanoTimestamp();
+    print("Answer: {d}\n", .{answer});
+    print(
+        "Took: {d} s, {d} ms, {d} us, {d} ns\n",
+        .{
+            @as(f64, @floatFromInt(end - start)) / 1_000_000_000,
+            @as(f64, @floatFromInt(end - start)) / 1_000_000,
+            @as(f64, @floatFromInt(end - start)) / 1_000,
+            end - start,
+        },
+    );
+}
