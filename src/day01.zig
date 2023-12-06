ac92a987c20699f6f972d001b90004f49ccfa595
Lewis Gaul <>
2023-12-06 20:46:42 +0000

    Remove unnecessary imports, format

--- src/day01.zig
+++ src/day01.zig
@@ -3,47 +3,15 @@
 // Only unmodified days will be updated.
 
 const std = @import("std");
-const Allocator = std.mem.Allocator;
-const List = std.ArrayList;
-const Map = std.AutoHashMap;
-const StrMap = std.StringHashMap;
-const BitSet = std.DynamicBitSet;
 const ParseIntError = std.fmt.ParseIntError;
-
-const util = @import("util.zig");
-const gpa = util.gpa;
-
-const data = @embedFile("data/day01.txt");
-
-// Useful stdlib functions
-const tokenizeAny = std.mem.tokenizeAny;
-const tokenizeSeq = std.mem.tokenizeSequence;
-const tokenizeSca = std.mem.tokenizeScalar;
-const splitAny = std.mem.splitAny;
-const splitSeq = std.mem.splitSequence;
-const splitSca = std.mem.splitScalar;
-const indexOf = std.mem.indexOfScalar;
-const indexOfAny = std.mem.indexOfAny;
-const indexOfStr = std.mem.indexOfPosLinear;
-const lastIndexOf = std.mem.lastIndexOfScalar;
-const lastIndexOfAny = std.mem.lastIndexOfAny;
-const lastIndexOfStr = std.mem.lastIndexOfLinear;
-const trim = std.mem.trim;
-const sliceMin = std.mem.min;
-const sliceMax = std.mem.max;
-
-const charToDigit = std.fmt.charToDigit;
-const parseInt = std.fmt.parseInt;
-const parseFloat = std.fmt.parseFloat;
-
+const isDigit = std.ascii.isDigit;
 const print = std.debug.print;
 const assert = std.debug.assert;
+const charToDigit = std.fmt.charToDigit;
 
-const sort = std.sort.block;
-const asc = std.sort.asc;
-const desc = std.sort.desc;
+const util = @import("util.zig");
 
-const isDigit = std.ascii.isDigit;
+const data = @embedFile("data/day01.txt");
 
 const Str = []const u8;
 
@@ -90,15 +58,15 @@ fn part1(input: Str) error{NoDigitFound}!u32 {
 
 fn strToNumber(str: Str) error{NoNumberFound}!u8 {
     const map = .{
-        .{"one", 1},
-        .{"two", 2},
-        .{"three", 3},
-        .{"four", 4},
-        .{"five", 5},
-        .{"six", 6},
-        .{"seven", 7},
-        .{"eight", 8},
-        .{"nine", 9},
+        .{ "one", 1 },
+        .{ "two", 2 },
+        .{ "three", 3 },
+        .{ "four", 4 },
+        .{ "five", 5 },
+        .{ "six", 6 },
+        .{ "seven", 7 },
+        .{ "eight", 8 },
+        .{ "nine", 9 },
     };
     inline for (map) |entry| {
         const diffIdx: ?usize = std.mem.indexOfDiff(u8, entry[0], str);
