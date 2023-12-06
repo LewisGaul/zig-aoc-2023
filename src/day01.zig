f2504212b7a91517415355d0fb6b8c2f64d40beb
Lewis Gaul <>
2023-12-06 20:44:04 +0000

    Day 1 part 2 done

--- src/day01.zig
+++ src/day01.zig
@@ -88,10 +88,49 @@ fn part1(input: Str) error{NoDigitFound}!u32 {
     return answer;
 }
 
+fn strToNumber(str: Str) error{NoNumberFound}!u8 {
+    const map = .{
+        .{"one", 1},
+        .{"two", 2},
+        .{"three", 3},
+        .{"four", 4},
+        .{"five", 5},
+        .{"six", 6},
+        .{"seven", 7},
+        .{"eight", 8},
+        .{"nine", 9},
+    };
+    inline for (map) |entry| {
+        const diffIdx: ?usize = std.mem.indexOfDiff(u8, entry[0], str);
+        if (diffIdx == null or diffIdx.? >= entry[0].len) return entry[1];
+    }
+    return error.NoNumberFound;
+}
+
+fn findNumber(line: Str, direction: FindDirection) error{NoNumberFound}!u8 {
+    for (0..line.len) |i| {
+        const idx = if (direction == .start) i else line.len - i - 1;
+        if (charToDigit(line[idx], 10) catch null) |d| return d;
+        if (strToNumber(line[idx..]) catch null) |n| return n;
+    }
+    return error.NoNumberFound;
+}
+
+fn part2(input: Str) error{NoNumberFound}!u32 {
+    var answer: u32 = 0;
+    var iter = LineIter.init(input);
+    while (iter.next()) |line| {
+        const first_num = try findNumber(line, .start);
+        const last_num = try findNumber(line, .end);
+        answer += 10 * first_num + last_num;
+    }
+    return answer;
+}
+
 pub fn main() !void {
     try util.runPart(part1, data);
     print("\n", .{});
-    // try util.runPart(part2, data);
+    try util.runPart(part2, data);
 }
 
 test "part1" {
@@ -103,4 +142,18 @@ test "part1" {
     ;
     const answer = try part1(input);
     assert(answer == 142);
+}
+
+test "part2" {
+    const input =
+        \\two1nine
+        \\eightwothree
+        \\abcone2threexyz
+        \\xtwone3four
+        \\4nineeightseven2
+        \\zoneight234
+        \\7pqrstsixteen
+    ;
+    const answer = try part2(input);
+    assert(answer == 281);
 }
